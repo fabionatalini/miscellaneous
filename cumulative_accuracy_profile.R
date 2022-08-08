2290b1b4b80b15ab2b48932dc30d2df709b7873b
Fabio Natalini <fabionatalini@gmail.com>
2022-08-08 16:27:57 +0200

    Update cumulative_accuracy_profile.R

--- cumulative_accuracy_profile.R
+++ cumulative_accuracy_profile.R
@@ -1,19 +1,18 @@
 cap_lift <- function(predictions,quantiles){
   #========================================================#
-  # predictions es un data.frame con dos columnas: 
-  # la primera columna con la respuesta real,
-  # la segunda columna con las predicciones;
-  # quantiles es el numero de cuantiles
-  # para segmentar el calculo de las metricas de evaluacion.
+  # predictions is a dataframe with two columns:
+  # the first column is the true response (binary numeric with unique values 0 and 1, where 1 is the positive response),
+  # the second column contains the probabilistic predictions;
+  # quantiles is the number of quantiles to calculate the results.
   #========================================================#
   # ordenar las predicciones
   set.seed(123)
   predictions$aleatory<-sample(1:nrow(predictions), nrow(predictions), replace=FALSE)
-  predictions<-predictions[order(predictions[,1], predictions$aleatory, decreasing = TRUE),]
+  predictions<-predictions[order(predictions[,2], predictions$aleatory, decreasing = TRUE),]
   # distribucion en cuantiles
   predictions$quantile<-as.numeric(cut(1:nrow(predictions),quantiles,labels=FALSE))
   # % de respuestas positivas capturadas en cada cuantil
-  Results <- aggregate(predictions[,2], by = list(predictions$quantile),  FUN = sum)
+  Results <- aggregate(predictions[,1], by = list(predictions$quantile),  FUN = sum)
   Results$sums_id <- aggregate(1:nrow(predictions), by = list(predictions$quantile),  FUN = length)[,"x"]
   Results$actual_accum <- cumsum(Results$x)
   Results$sums_id_accum <- cumsum(Results$sums_id)
